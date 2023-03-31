c3ed00092944ea3e537c22290118fa5cbe5c6ab4
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-03-31 16:00:09 +0300

    result task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
 
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+inputEl.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+function inputCountry(e) {
+  const name = inputEl.value.trim();
+
+  fetchCountries(name).then(renderInfoCountry).catch(showError);
+  cleanHtml();
+}
+
+function renderInfoCountry() {
+  const name = inputEl.value.trim();
+  if (name !== '') {
+    fetchCountries(name).then(name => {
+      if (name.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (name.length === 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      } else if (name.length >= 2 && name.length <= 10) {
+        renderCountryList(name);
+      } else if (name.length === 1) {
+        renderOneCountry(name);
+      }
+    });
+  }
+}
+
+function renderCountryList(country) {
+  const markup = country
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+         <h2>${country.name.official}</h2>
+                </li>`;
+    })
+    .join('');
+  listEl.innerHTML = markup;
+  listEl.li.style.textDecoration = 'none';
+}
+
+function renderOneCountry(country) {
+  const markup = country
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <b>${country.name.official}</b></p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+    })
+    .join('');
+  listEl.innerHTML = markup;
+}
+function showError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+function cleanHtml() {
+  listEl.innerHTML = '';
+  infoEl.innerHTML = '';
+}
